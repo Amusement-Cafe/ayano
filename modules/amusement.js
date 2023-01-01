0e2f9c864d61930030976220e2f37e7118a85b6b
Mj11jM <themj11jm@gmail.com>
2023-01-01 03:24:42 -0800

    Transfer Amusement over to child process

--- modules/amusement.js
+++ modules/amusement.js
@@ -2,7 +2,8 @@
 * Support for starting and managing amusement club cluster
 * Used by CLI and bot
 */
- 
+
+const child           = require('child_process')
 const { cmd, pcmd }   = require('../core/cmd')
 const events          = require('../core/events')
 
@@ -12,33 +13,42 @@ const {
     withCLI 
 }  = require('../core/with')
 
-var instance, connected, amusement
+let instance, connected, amusement, options, evs
 
-const create = async (ctx) => {
+const delay = time => new Promise(res=>setTimeout(res,time));
 
-    amusement = require('amusementclub2.0')
+const create = (ctx) => {
+
     ctx.allowExit = false
-    const options  = Object.assign({shards: ctx.config.shards, data: ctx.data}, ctx.config.shard)
-    instance = await amusement.create(options)
+    options  = Object.assign({shards: ctx.config.shards, data: ctx.data}, ctx.config.shard)
+    instance = child.fork('../amusementclub2.0', {env: options})
+    instance.send({startup: options})
 
-    instance.emitter.on('info', (msg, title) => ctx.info(`[Amusement] ${msg}`, title))
-    instance.emitter.on('error', ctx.error)
+    instance.on('message', (msg) => {
+        if (msg.info) ctx.info(`[Amusement] ${msg.info}`)
+        if (msg.error) ctx.error(msg.error)
+    })
+    instance.on('info', (msg, title) => ctx.info(`[Amusement] ${msg}`, title))
+    instance.on('error', ctx.error)
 
-    events.on('quit', () => disconnect(ctx))
-    events.on('colupdate', (data) => instance.updateCols(data))
-    events.on('cardupdate', (data) => instance.updateCards(data))
-    events.on('promoupdate', (data) => instance.updatePromos(data))
-    events.on('boostupdate', (data) => instance.updateBoosts(data))
-    events.on('wordsupdate', (data) => instance.updateWords(data))
+    if (!evs) {
+        events.on('quit', () => disconnect(ctx))
+        events.on('colupdate', (data) => instance.send({updateCols: data}))
+        events.on('cardupdate', (data) => instance.send({updateCards: data}))
+        events.on('promoupdate', (data) => instance.send({updatePromos: data}))
+        events.on('boostupdate', (data) => instance.send({updateBoosts: data}))
+        events.on('wordsupdate', (data) => instance.send({updateWords: data}))
+        evs = true
+    }
+
 }
 
 const startBot = async (ctx) => {
     if(connected)
         return await ctx.error(`**Amusement bot** is already running`)
-    
-    if(!instance) await create(ctx)
 
-    await instance.connect()
+    await ctx.info(`**Amusement bot** is starting`)
+    if(!instance) create(ctx)
     connected = true
 }
 
@@ -46,18 +56,19 @@ const disconnect = async (ctx) => {
     if(!connected)
         return await ctx.error(`**Amusement bot** is not running`)
 
-    await instance.disconnect()
+    instance.send({shutdown: true})
+    await delay(1000)
     await ctx.warn(`**Amusement bot** was disconnected`)
     connected = false
+    instance = null
 }
-
-const delay = time => new Promise(res=>setTimeout(res,time));
 
 const reconnect = async (ctx) => {
     if(!connected)
         return await ctx.error(`**Amusement bot** is not running`)
 
     await disconnect(ctx)
+    await delay(1500)
     await startBot(ctx)
     await ctx.info(`Restarted Amusement bot connection to Discord`)
 }
