ae5629db264b7cae3d1177810cc76a875ab72424
Mj11jM <themj11jm@gmail.com>
2023-03-25 14:20:38 -0700

    Add auto restart to amusement

--- modules/amusement.js
+++ modules/amusement.js
@@ -13,12 +13,11 @@ const {
     withCLI 
 }  = require('../core/with')
 
-let queue = [], instance, connected, options, evs
+let queue = [], instance, connected, options, evs, ars
 
 const delay = time => new Promise(res=>setTimeout(res,time))
 
 const create = (ctx) => {
-
     ctx.allowExit = false
     options  = Object.assign({data: ctx.data}, ctx.config.amusement)
     instance = child.fork('../amusementclub2.0', {env: ctx.config.amusement.bot})
@@ -50,6 +49,27 @@ const startBot = async (ctx) => {
     await ctx.info(`**Amusement bot** is starting`)
     if(!instance) create(ctx)
     connected = true
+}
+
+const autoRestart = async (ctx) => {
+    await ctx.info('Starting the autorestart!')
+    instance.send({autorestart: true})
+
+    await delay(175000)
+
+    await ctx.warn(`**Amusement bot** was disconnected`)
+    connected = false
+    instance = null
+    await delay(1000)
+    await startBot(ctx)
+}
+
+const startAutoRestart = async (ctx) => {
+    if (!connected)
+        return await ctx.error(`**Amusement bot** is not running`)
+
+    ars = setInterval(() => autoRestart(ctx), 604800 * 1000)
+    await ctx.info('**Amusement Bot** auto restart has been enabled!')
 }
 
 const disconnect = async (ctx) => {
@@ -103,6 +123,7 @@ pcmd(['admin'],['start'], withCLI(withConfig(withData(startBot))))
 pcmd(['admin'],['stop'], disconnect)
 pcmd(['admin'],['restart'], restart)
 pcmd(['admin'], ['git', 'pull'], gitPull)
+pcmd(['admin'], ['autorestart'], startAutoRestart)
 
 module.exports = {
     addVoteQueue
